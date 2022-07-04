96fa75e7571b2431ad807cfabd40932546bdd750
Sudhanva M <91898207+sud0x00@users.noreply.github.com>
2022-07-04 14:54:19 +0530

    Create encode.py

---
+++ encode.py
@@ -0,0 +1,3 @@
+text = "Moskau , , , ,  Fremd und geheimnisvoll Türme aus rotem Gold Kalt wie das Eis....... Moskau Doch wer dich wirklich kennt Der weiß, wenn Feuer brennt In dir so heiß!!!!!!"
+Text = text.encode("utf-8")
+print(Text)
